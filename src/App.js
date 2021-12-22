533898e8472ca7946155300aae08f03e5c1f4091
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-22 15:06:44 -0500

    add handleRed function and test onClick

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [color, setColor] = useState({ backgroundColor: "#111" });
+  const [color, setColor] = useState("#111");
+
+  const handleRed = () => {
+    setColor("red");
+  };
 
   return (
     <div className="App">
       <section>
         <div id="controlPanel">
-          <h2 id="stopButton" className="button">
+          <h2 id="stopButton" className="button" onClick={handleRed}>
             Stop
           </h2>
           <h2 id="slowButton" className="button">
@@ -19,9 +23,21 @@ export default function App() {
           </h2>
         </div>
         <div id="traffic-light">
-          <div id="stopLight" style={color} className="bulb"></div>
-          <div id="slowLight" style={color} className="bulb"></div>
-          <div id="goLight" style={color} className="bulb"></div>
+          <div
+            id="stopLight"
+            style={{ backgroundColor: color }}
+            className="bulb"
+          ></div>
+          <div
+            id="slowLight"
+            style={{ backgroundColor: color }}
+            className="bulb"
+          ></div>
+          <div
+            id="goLight"
+            style={{ backgroundColor: color }}
+            className="bulb"
+          ></div>
         </div>
       </section>
     </div>
