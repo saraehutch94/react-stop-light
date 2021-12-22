d31e3081209dc62b2afabf5ed44570ab8df38303
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-22 14:19:56 -0500

    add App.js return statement and starter css code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <section>
+        <div id="controlPanel">
+          <h2 id="stopButton" className="button">
+            Stop
+          </h2>
+          <h2 id="slowButton" className="button">
+            Slow
+          </h2>
+          <h2 id="goButton" className="button">
+            Go
+          </h2>
+        </div>
+        <div id="traffic-light">
+          <div id="stopLight" className="bulb"></div>
+          <div id="slowLight" className="bulb"></div>
+          <div id="goLight" className="bulb"></div>
+        </div>
+      </section>
     </div>
   );
 }
