033e422e0b77b5c545f1969a82d0166aa457401b
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-26 12:09:53 -0500

    refactor code: one useState, one handleLight function / test e.target

--- src/App.js
+++ src/App.js
@@ -2,56 +2,66 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [red, setRed] = useState("#111");
-  const [yellow, setYellow] = useState("#111");
-  const [green, setGreen] = useState("#111");
+  // const [red, setRed] = useState("#111");
+  // const [yellow, setYellow] = useState("#111");
+  // const [green, setGreen] = useState("#111");
 
-  const handleRed = () => {
-    setRed("red");
-    setYellow("#111");
-    setGreen("#111");
+  const [lightState, setLight] = useState({
+    red: "#111",
+    yellow: "#111",
+    green: "#111"
+  });
+
+  const handleLight = (e) => {
+    console.log(e.target);
   };
 
-  const handleYellow = () => {
-    setYellow("yellow");
-    setRed("#111");
-    setGreen("#111");
-  };
+  // const handleRed = () => {
+  //   setRed("red");
+  //   setYellow("#111");
+  //   setGreen("#111");
+  // };
 
-  const handleGreen = () => {
-    setGreen("green");
-    setRed("#111");
-    setYellow("#111");
-  };
+  // const handleYellow = () => {
+  //   setYellow("yellow");
+  //   setRed("#111");
+  //   setGreen("#111");
+  // };
+
+  // const handleGreen = () => {
+  //   setGreen("green");
+  //   setRed("#111");
+  //   setYellow("#111");
+  // };
 
   return (
     <div className="App">
       <section>
         <div id="controlPanel">
-          <h2 id="stopButton" className="button" onClick={handleRed}>
+          <h2 id="stopButton" className="button" onClick={handleLight}>
             Stop
           </h2>
-          <h2 id="slowButton" className="button" onClick={handleYellow}>
+          <h2 id="slowButton" className="button" onClick={handleLight}>
             Slow
           </h2>
-          <h2 id="goButton" className="button" onClick={handleGreen}>
+          <h2 id="goButton" className="button" onClick={handleLight}>
             Go
           </h2>
         </div>
         <div id="traffic-light">
           <div
             id="stopLight"
-            style={{ backgroundColor: red }}
+            style={{ backgroundColor: lightState.red }}
             className="bulb"
           ></div>
           <div
             id="slowLight"
-            style={{ backgroundColor: yellow }}
+            style={{ backgroundColor: lightState.yellow }}
             className="bulb"
           ></div>
           <div
             id="goLight"
-            style={{ backgroundColor: green }}
+            style={{ backgroundColor: lightState.green }}
             className="bulb"
           ></div>
         </div>
